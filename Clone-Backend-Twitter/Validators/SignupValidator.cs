2cac660d25a6f4dfab79e634f8697e674d3d69c4
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-01-05 20:31:53 -0300

    feat: Cria Validator para validação dos dados ao registrar novos usuários

---
+++ Clone-Backend-Twitter/Validators/SignupValidator.cs
@@ -0,0 +1,22 @@
+﻿using Clone_Backend_Twitter.Models.Dto;
+using FluentValidation;
+
+namespace Clone_Backend_Twitter.Validators;
+
+public class SignupValidator : AbstractValidator<SignupDto>
+{
+    public SignupValidator()
+    {
+        RuleFor(signup => signup.Name)
+            .NotEmpty().WithMessage("Nome é obrigatório!")
+            .MinimumLength(2).WithMessage("O nome deve ter pelo menos 2 caracteres!");
+
+        RuleFor(signup => signup.Email)
+            .NotEmpty().WithMessage("Email é obrigatório!")
+            .EmailAddress().WithMessage("Email inválido!");
+        
+        RuleFor(signup => signup.Password)
+            .NotEmpty().WithMessage("Senha é obrigatória!")
+            .MinimumLength(6).WithMessage("A senha deve ter pelo menos 6 caracteres!");
+    }
+}
