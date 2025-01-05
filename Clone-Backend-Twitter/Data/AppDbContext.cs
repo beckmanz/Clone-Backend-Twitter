aa282d314f55eba1a6bc6272b1fec0d6284fa093
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-01-05 00:19:16 -0300

    feat: faz configuração inicial, configura conexão com banco de dados, instala packages e etc

---
+++ Clone-Backend-Twitter/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Clone_Backend_Twitter.Data.Map;
+using Clone_Backend_Twitter.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clone_Backend_Twitter.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+}
