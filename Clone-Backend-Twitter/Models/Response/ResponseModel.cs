6053aa9b7d2df622437e2533b87e994a40e85aa1
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-01-05 20:28:38 -0300

    feat: cria ResponseModel para padronizar as respostas da api.

---
+++ Clone-Backend-Twitter/Models/Response/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Clone_Backend_Twitter.Models.Response;
+
+public class ResponseModel<T>
+{
+    public string Message { get; set; } = string.Empty;
+    public Boolean Status { get; set; } = true;
+    public T? Data { get; set; }
+}
